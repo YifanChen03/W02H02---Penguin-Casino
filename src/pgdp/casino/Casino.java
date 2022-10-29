9926a7ef21780786f33d3db36596d7c1173dc917
cyanfhine <yifan03.chen@tum.de>
2022-10-29 18:58:27 +0200

    1.0

--- src/pgdp/casino/Casino.java
+++ src/pgdp/casino/Casino.java
@@ -7,13 +7,27 @@ import java.io.InputStreamReader;
 public class Casino {
 
 	public static void penguBlackJack() {
-
 		// Here is a card deck for your games :)
 		// Remember for testing you can use seeds:
 		// CardDeck deck = CardDeck.getDeck(420);
 		CardDeck deck = CardDeck.getDeck();
+		int tokens = 0;
+		int input = 0;
 
-		// TODO
+		tokens = set_tokens(1000);
+		input = startGame();
+		if (input == 1) {
+			//whatever happens after 1
+		} else {
+			//whatever happens after 2
+			System.out.println("Your final balance: " + tokens);
+			if (tokens > 1000) {
+				System.out.println("Wohooo! Ez profit! :D");
+			} else {
+				System.out.println("That's very very sad :(");
+			}
+			System.out.println("Thank you for playing. See you next time.");
+		}
 	}
 
 	public static String readString() {
@@ -35,6 +49,24 @@ public class Casino {
 				System.out.println("This was not a valid number, please try again.");
 			}
 		}
+	}
+
+	public static int startGame() {
+		int input = 0;
+		System.out.println("Welcome to Pengu-BlackJack!");
+		do {
+			System.out.println("(1) Start a game or (2) exit");
+			input = readInt();
+			if (input != 1 && input !=2) {
+				System.out.println("What?!");
+			}
+		} while (input != 1 && input !=2);
+		return input;
+	}
+
+	public static int set_tokens(int n) {
+		int tokens = n;
+		return tokens;
 	}
 
 	public static void main(String[] args) {
